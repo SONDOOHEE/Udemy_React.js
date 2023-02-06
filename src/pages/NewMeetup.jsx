2a57b63c561c318133239653d1dc65df4ceef462
SONDOOHEE <doohee456@naver.com>
2023-02-06 13:44:56 +0900

    [#42]POST HTTP request

--- src/pages/NewMeetup.jsx
+++ src/pages/NewMeetup.jsx
@@ -1,13 +1,21 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm"
 
 const NewMeetup = () => {
+  const addMeetupHandler = (meetupData) => {
+    fetch('https://react-getting-started-f9410-default-rtdb.firebaseio.com/meetups.json',
+    {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  }
   return (
-    <>
     <section>
       <h1> Add New Meetup </h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler}/>
     </section>
-    </>
   )
 }
 
